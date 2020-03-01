2e6205edc4b9f884c3786da4e6e80dbea6fc7ad1
Your Name <you@example.com>
2020-03-01 14:24:30 +0900

    divisor, prime_factor

---
+++ b033arc.py
@@ -0,0 +1,82 @@
+import sys
+sys.setrecursionlimit(4100000)
+
+import math
+
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+
+logger = logging.getLogger(__name__)
+
+# from collections import defaultdict
+# d = defaultdict(list)
+
+# from itertools import combinations
+# comb = combinations(range(N), 2)
+
+# 累積和
+# from itertools import accumulate
+# _list = list(accumulate(a_list)
+
+# from functools import lru_cache
+# @lru_cache(maxsize=None)
+# def setUp(self):
+#     dp.cache_clear()
+
+
+def resolve():
+    # S = [x for x in sys.stdin.readline().split()][0]  # 文字列 一つ
+    # N = [int(x) for x in sys.stdin.readline().split()][0]  # int 一つ
+    NA, NB = [int(x) for x in sys.stdin.readline().split()]  # 複数int
+    a_list = [int(x) for x in sys.stdin.readline().split()]  # 複数int
+    b_list = [int(x) for x in sys.stdin.readline().split()]  # 複数int
+
+    # grid = [list(sys.stdin.readline().split()[0]) for _ in range(N)]  # 文字列grid
+    # v_list = [int(sys.stdin.readline().split()[0]) for _ in range(N)]
+    # grid = [[int(x) for x in sys.stdin.readline().split()]
+    #         for _ in range(N)]  # int grid
+
+    logger.debug('{}'.format([]))
+
+    a = set(a_list)
+    b = set(b_list)
+
+    print(len(a & b) / len(a | b))
+
+
+if __name__ == "__main__":
+    resolve()
+
+# AtCoder Unit Test で自動生成できる, 最後のunittest.main は消す
+# python -m unittest template/template.py で実行できる
+# pypy3 -m unittest template/template.py で実行できる
+
+import sys
+from io import StringIO
+import unittest
+
+
+class TestClass(unittest.TestCase):
+    def assertIO(self, input, output):
+        stdout, stdin = sys.stdout, sys.stdin
+        sys.stdout, sys.stdin = StringIO(), StringIO(input)
+        resolve()
+        sys.stdout.seek(0)
+        out = sys.stdout.read()[:-1]
+        sys.stdout, sys.stdin = stdout, stdin
+        self.assertEqual(out, output)
+
+    def test_入力例1(self):
+        input = """3 2
+1 3 5
+1 2"""
+        output = """0.2500000000"""
+        self.assertIO(input, output)
+
+    def test_入力例2(self):
+        input = """9 10
+11 2 33 4 55 6 77 8 99
+10 11 14 19 55 1000000000 4 5 7 8"""
+        output = """0.2666666667"""
+        self.assertIO(input, output)
--- test.py
+++ test.py
@@ -1,6 +1,4 @@
 import random
 # N = 2000
-N = 3
-print(N, N)
-for i in range(N):
-    print(' '.join([str(random.randint(0, 9)) for _ in range(N)]))
+N = 150
+print(''.join([random.choice(['i', 'w']) for _ in range(N)]))
