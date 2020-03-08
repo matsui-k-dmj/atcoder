df5195846c7a8c8434a8f38158c3d3393bdeb5b7
Your Name <you@example.com>
2020-03-08 20:52:22 +0900

    abc158

--- test.py
+++ test.py
@@ -1,14 +1,6 @@
-import random
-import string
-# N = 2000
-N = 500000
-print(N)
-print(''.join([random.choice(string.ascii_lowercase) for _ in range(N)]))
-Q = 20000
+S = 'a' * 10**5
+Q = 2 * 10**5
+print(S)
 print(Q)
-for _ in range(Q // 2):
-    print("{} {} {}".format(1, random.randint(1, N),
-                            random.choice(string.ascii_lowercase)))
-for _ in range(Q // 2):
-    print("{} {} {}".format(2, random.randint(1, N // 2),
-                            random.randint(N // 2, N)))
+for _ in range(Q):
+    print('2 1 z')
